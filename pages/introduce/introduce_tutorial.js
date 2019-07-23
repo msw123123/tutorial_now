aaf1eaae6a62ec6ea95348867e70f952ca87005e
msw123123 <sw.moon@specupad.com>
2019-07-23 22:34:39 +0900

    now api

--- pages/introduce/introduce_tutorial.js
+++ pages/introduce/introduce_tutorial.js
@@ -4,40 +4,53 @@ import Head from 'next/head'
 import css from "../../static/css/style.css"
 import Axios from 'axios';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>NOW 튜토리얼</title>
-    </Head>
 
-    { this }
-    <div className={css.head}>
-      <h1 className={css.title}>NOW 튜토리얼</h1>
 
-      <div className={css.content}>
-        <div className={css.codeFormat}>
-          <span className={css.codeContent}>npm i -g now</span>
+class Tutorial extends React.Component {
+  // var data = {}
+  // Axios.get('/api/date').then(function (response){
+  //   data = response.data
+  //   console.log(data)
+  // })
+
+  static async getInitialProps ({req}) {
+    const response = await Axios.get('/api/date');
+    return {
+        data: response.data
+    }
+  }
+
+  render() {
+    const { users } = this.props;
+    console.log(users)
+      return (
+        <div>
+        <Head>
+          <title>NOW 튜토리얼</title>
+        </Head>
+
+        { this }
+        <div className={css.head}>
+          <h1 className={css.title}>NOW 튜토리얼</h1>
+
+          <div className={css.content}>
+            <div className={css.codeFormat}>
+              <span className={css.codeContent}>npm i -g now</span>
+            </div>
+            <div className={css.codeFormat}>
+              <span className={css.codeContent}>now login</span>
+            </div>
+            <div className={css.codeFormat}>
+              <span className={css.codeContent}>now</span>
+            </div>
+          </div>
         </div>
-        <div className={css.codeFormat}>
-          <span className={css.codeContent}>now login</span>
-        </div>
-        <div className={css.codeFormat}>
-          <span className={css.codeContent}>now</span>
-        </div>
+
+        <style jsx>{`
+
+        `}</style>
       </div>
-    </div>
-
-    <style jsx>{`
-
-    `}</style>
-  </div>
-)
-
-var data = {}
-Axios.get('/api/date').then(function (response){
-  data = response.data
-  console.log(data)
-})
-
-
-export default Home
+      );
+  }
+}
+export default Tutorial
